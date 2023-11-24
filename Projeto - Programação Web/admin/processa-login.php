affeac24f84d970c99a7443b3920939edb3585c9
Thiago Schiedeck <103116333+oThisgo@users.noreply.github.com>
2023-11-24 20:18:36 -0300

    Add files via upload

--- Projeto - Programação Web/admin/processa-login.php
+++ Projeto - Programação Web/admin/processa-login.php
@@ -2,20 +2,28 @@
 
 include_once '../Includes/db.php';
 
-$user = "SELECT * FROM admins WHERE usuario = '".$_POST['user']."'";
-$result = mysqli_query($db, $user);
+if (isset($_POST['acessar']) && !empty($_POST['user'] && !empty($_POST['pass']))) {
+    $user = $_POST['user'];
+    $pass = $_POST['pass'];
+    $query = "SELECT * FROM admins WHERE usuario = '$user' AND senha = '$pass'";
+    $command = mysqli_query($db, $query);
+    $result = mysqli_fetch_assoc($command);
 
-if (mysqli_num_rows($result) == 1) {
-    $senha = "SELECT senha FROM admins WHERE usuario = '".$_POST['user']."'";
-    $pass = mysqli_query($db, $senha);
-
-    if ($_POST['pass'] == $pass) {
+    if (!empty($result['usuario'])) {
+        session_start();
+        $_SESSION['nome'] = $result['nome']; 
+        $_SESSION['id'] = $result['userID'];
+        $_SESSION['ativa'] = TRUE;
         header('Location: painel.php');
+        
     } else {
-        echo 'Algo deu errado :(';
+        echo "Usuário ou senha incorretos!";
     }
+
 } else {
-    echo "Usuário ou senha incorretos.";
+    echo "Digite valores válidos e tente novamente!";
 }
+
+
 
 ?>
